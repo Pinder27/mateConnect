175c732fa8ef75a80358a5fb53bdeb0b6cb1c33e
Pinder27 <pinders647@gmail.com>
2024-04-23 11:43:59 +0530

    ef-16

--- src/app/flatmates/page.tsx
+++ src/app/flatmates/page.tsx
@@ -3,9 +3,7 @@ import FlatMateList from "../components/lib/flatmate/FlatMateList/page"
 
  
 export default async function Page() {
-    console.log("nextauth yrl",process.env.NEXTAUTH_URL);
-    console.log("nextauth secrtet",process.env.NEXTAUTH_SECRET);
-    console.log("urldb",process.env.DATABASE_URL);
+    
     return(
         <div className="h-screen">
             <AppBar/>
