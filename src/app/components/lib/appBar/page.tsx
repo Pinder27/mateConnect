cccfe0c97fd5eb37c85ae7ac4bdf8611f96437fd
Pinder27 <pinders647@gmail.com>
2024-05-27 02:24:58 +0530

    login page redesigned

--- src/app/components/lib/appBar/page.tsx
+++ src/app/components/lib/appBar/page.tsx
@@ -15,12 +15,12 @@ export default function Page({session}:{session:any}){
         <div className="flex flex-row border-b-4 mb-3 items-center p-4 bg-white">
             <Link className="font-bold text-xl text-purple-900 italic" href={"/"}>FlatMates</Link>
             <div className="ms-auto flex me-10">
-            {session&&<Link href={'/flatmates/AddPost'} className="opacity-75 rounded me-8 ms-auto p-2 bg-purple-900 hover:bg-lime-700 active:bg-purple-800 text-white">Add Post</Link>}
+            {session&&<Link href={'/flatmates/AddPost'} className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 opacity-75 rounded me-8 ms-auto p-2 bg-purple-900 hover:bg-purple-950 active:bg-purple-800 text-white">Add Post</Link>}
             {session && (    
                     <DropdownButton/>
             )}
             {!session && (
-                <button className=" ms-auto p-2 px-4 bg-purple-800 hover:bg-purple-900 active bg-blue-400 rounded text-white" onClick={(e) => {e.preventDefault();signIn()}}>
+                <button className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  ms-auto p-2 px-4 bg-purple-800 hover:bg-purple-900 active bg-blue-400 rounded text-white" onClick={(e) => {e.preventDefault();signIn()}}>
                     login
                 </button>
             )}
--- src/app/login/loginForm/page.tsx
+++ src/app/login/loginForm/page.tsx
@@ -6,68 +6,79 @@ import { useRouter } from "next/navigation";
 import { FormEvent, useState } from "react";
 
 
- 
-export default function Page(){
-    const [error,setError] = useState<string | null>(null)  
-    const [view,setView] = useState<boolean>(false)
+
+
+export default function Page() {
+    const [error, setError] = useState<string | null>(null)
+    const [view, setView] = useState<boolean>(false)
     const router = useRouter()
 
-    const handleSubmit = async(e:FormEvent<HTMLFormElement>)=>{
-            e.preventDefault();
-            const formdata = new FormData(e.currentTarget);
-          const res = await  signIn("credentials",{
-                email:formdata.get("email"),
-                password:formdata.get("password"),
-                redirect:false
-            })
-           
-           if(res?.error) setError(res.error)
-            if(res?.status == 200) router.push('/')
-            
-    }
-    return(
-        <div className="bg-grey-lighter min-h-screen flex flex-col">
-            <Link className="m-4 font-bold text-xl text-purple-900 italic" href={"/"}>FlatMates</Link>
-                    <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-                        <form onSubmit={handleSubmit} className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-                            <h1 className="mb-8 text-3xl text-center">Login</h1>
-                            
-                        {  error && <div className="text-red-500 text-center">{error}</div>}
-                            <input 
-                                type="text"
-                                required
-                                className="outline-none block border border-grey-light w-full p-3 rounded mb-4"
-                                name="email"
-                                placeholder="Email" />
-                            <div className="relative mb-4">
-                            <input 
-                                type={!view?'password':'text'}
-                                required
-                                className="outline-none block border border-grey-light w-full p-3 rounded "
-                                name="password"
-                                placeholder="Password" />
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formdata = new FormData(e.currentTarget);
+        const res = await signIn("credentials", {
+            email: formdata.get("email"),
+            password: formdata.get("password"),
+            redirect: false
+        })
 
-                                <button type="button" onMouseUp={()=>setView(false)} onMouseDown={()=>setView(true)}>
-                                <img src="/eye.png" alt="eye" className="absolute right-3 top-4  w-5 h-5"/>
-                                </button>
-                                </div>
-                        
+        if (res?.error) setError(res.error)
+        if (res?.status == 200) router.push('/')
+
+    }
+    return (
         
-                            <button
-                                type="submit"
-                                className="w-full text-center py-3 rounded bg-green-500 text-white hover:bg-green-600 focus:outline-none my-1"
-                            >Login
-                            </button>
-        
-                        </form>
-        
-                        <div className="text-grey-dark mt-6">
-                            New User? 
-                            <Link className="ms-2 no-underline border-b border-blue text-blue-500" href="/signup">
-                                Sign up
-                            </Link>.
-                        </div>
+        <div className=" bg-zinc-300 h-screen flex flex-col" >
+            <Link className="m-4 font-bold text-xl text-purple-900 italic" href={"/"}>FlatMates</Link>
+           <div className="flex-1 flex flex-col justify-center items-center content-center">
+            <div className="rounded bg-fixed bg-contain flex justify-center items-center w-2/3 h-5/6 mx-auto" style={{backgroundImage:"url(/img-1.jpg)"}}>
+            <div className=" "  >
+                
+                <form onSubmit={handleSubmit} className="backdrop-blur-sm bg-transparent	 mx-auto bg-white   px-6 py-8 rounded shadow-md text-black w-80">
+                    <h1 className="text-white mb-8 text-3xl text-center">Login</h1>
+
+                    {error && <div className="text-red-500 text-lg text-center">{error}</div>}
+                    <input
+                        type="text"
+                        required
+                        style={{background:"rgba(255, 255, 255, 0.6)"}}
+                        className="placeholder-gray-700 outline-none block border  w-full p-3 rounded mb-4"
+                        name="email"
+                        placeholder="Email" />
+                    <div className="relative mb-4">
+                        <input
+                            type={!view ? 'password' : 'text'}
+                            required
+                            style={{background:"rgba(255, 255, 255, 0.6)"}}
+                            className="placeholder-gray-700 outline-none block border border-grey-light w-full p-3 rounded "
+                            name="password"
+                            placeholder="Password" />
+
+                        <button type="button" onMouseUp={() => setView(false)} onMouseDown={() => setView(true)}>
+                            <img src="/eye.png" alt="eye" className="absolute right-3 top-4  w-5 h-5" />
+                        </button>
                     </div>
+
+                    <button
+                        type="submit"
+                        className=" w-full text-center py-3 rounded bg-green-500 text-white hover:bg-green-600 focus:outline-none my-1"
+                    >
+                        Login
+                    </button>
+                    <div className="text-white mt-6 text-center">
+                    New User?
+                    <Link className="ms-2 no-underline border-b border-blue text-blue-500" href="/signup">
+                        Sign up
+                    </Link>.
                 </div>
+                </form>
+              
+                </div>
+                </div>
+               
+            </div>
+            
+        </div>
+        
     )
 }
